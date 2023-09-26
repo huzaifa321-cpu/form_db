a497daf26d376fbc1a6229a85558b61166a7d70b
huzaifa321-cpu <101012218+huzaifa321-cpu@users.noreply.github.com>
2023-09-26 23:44:52 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.Label();
+            this.Company = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(138, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(332, 401);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(577, 143);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(113, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(113, 72);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Location = new System.Drawing.Point(402, 15);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(386, 96);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 230);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 150);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // ID
+            // 
+            this.ID.AutoSize = true;
+            this.ID.Location = new System.Drawing.Point(46, 32);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(18, 13);
+            this.ID.TabIndex = 7;
+            this.ID.Text = "ID";
+            this.ID.Click += new System.EventHandler(this.ID_Click);
+            // 
+            // Company
+            // 
+            this.Company.AutoSize = true;
+            this.Company.Location = new System.Drawing.Point(49, 78);
+            this.Company.Name = "Company";
+            this.Company.Size = new System.Drawing.Size(51, 13);
+            this.Company.TabIndex = 8;
+            this.Company.Text = "Company";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Company);
+            this.Controls.Add(this.ID);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label ID;
+        private System.Windows.Forms.Label Company;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine("Form1_Load() called...");
+            richTextBox1.Text = "Startup..."; try
+            {
+                System.Diagnostics.Debug.WriteLine("within the try");
+                SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-8LKHOJB\SQLEXPRESS;Trusted_Connection=True;Initial Catalog=Northwind;Integrated Security=True");
+                connection.Open();
+                richTextBox1.Text = "Connection Successful";
+                connection.Close();
+            }
+
+            catch (Exception ex)
+            {
+                richTextBox1.Text = "Error, " + ex;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand command = new SqlCommand();
+            SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-8LKHOJB\SQLEXPRESS;Trusted_Connection=True;Initial Catalog=Northwind;Integrated Security=True");
+            connection.Open();
+            richTextBox1.Text = "Inserting Record...";
+            command.Connection = connection;
+            command.CommandText = "insert into Customers (ID, Company) values('" +
+ID.Text + "','" + Company.Text + "')";
+            command.ExecuteNonQuery();
+            richTextBox1.Text = "Record Inserted...";
+            connection.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlCommand command = new SqlCommand();
+            SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-8LKHOJB\SQLEXPRESS;Trusted_Connection=True;Initial Catalog=Northwind;Integrated Security=True");
+            connection.Open();
+            richTextBox1.Text = "Counting Records...";
+            command.Connection = connection;
+            command.CommandText = "select count(*) from Customers";
+            int count = (int)command.ExecuteScalar();
+            richTextBox1.Text = "Number of records: " + count;
+            connection.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlCommand command = new SqlCommand();
+            SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-8LKHOJB\SQLEXPRESS;Trusted_Connection=True;Initial Catalog=Northwind;Integrated Security=True");
+            connection.Open();
+            richTextBox1.Text = "Retrieving Records...";
+            command.Connection = connection;
+            command.CommandText = "select * from Customers";
+            SqlDataAdapter da = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            richTextBox1.Text = "Retrieval Successful!";
+            connection.Close();
+        }
+
+        private void ID_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
